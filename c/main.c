8e21fa1701793b7ff56125da3f52c59ad4869b16
Kyle Fredrickson <kyfredri@ucsc.edu>
2021-01-26 16:55:57 -0800

    Added c implementation of speck.

---
+++ c/main.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "speck.h"
+
+int main()
+{
+    uint64_t speck_key[4] = {0x0706050403020100UL,0x0f0e0d0c0b0a0908UL,0x1716151413121110UL, 0x1f1e1d1c1b1a1918UL};
+    uint64_t speck_pt[2] = {0x202e72656e6f6f70UL, 0x65736f6874206e49UL};
+    uint64_t speck_ct[2] = {0x4eeeb48d9c188f43UL, 0x4109010405c0f53eUL};
+    uint64_t test[2] = {0UL, 0UL};
+
+    speck_encrypt(speck_pt, test, speck_key);
+    int speck_test = 1;
+    for (int i = 0; i < 2; i++)
+    {
+        speck_test = speck_test && (speck_ct[i] == test[i]);
+    }
+    speck_decrypt(speck_ct, test, speck_key);
+    for (int i = 0; i < 2; i++)
+    {
+        speck_test = speck_test && (speck_pt[i] == test[i]);
+    }
+    if (!speck_test)
+        return 0;
+    else
+        return 1;
+}
---
+++ c/speck.h
@@ -0,0 +1,11 @@
+#ifndef SPECK
+#define SPECK
+
+#include <stdint.h>
+#include <stdlib.h>
+
+void speck_ctr(uint64_t *in, uint64_t *out, size_t pt_length, uint64_t *key, uint64_t *nonce);
+void speck_encrypt(uint64_t *in, uint64_t *out, uint64_t *key);
+void speck_decrypt(uint64_t *in, uint64_t *out, uint64_t *key);
+
+#endif
